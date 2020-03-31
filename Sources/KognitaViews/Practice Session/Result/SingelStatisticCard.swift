125a965d86503c86152530b831fa1bee823d9af7
snegard <stiande@stud.ntnu.no>
2020-03-31 13:44:40 +0200

    [Feature] Added a card on practise session results with time used.

--- Sources/KognitaViews/Practice Session/Result/SingelStatisticCard.swift
+++ Sources/KognitaViews/Practice Session/Result/SingelStatisticCard.swift
@@ -4,44 +4,34 @@ import BootstrapKit
 struct SingleStatisticCardContent {
     let title: String
     let mainContent: String
-    let extraContent: String?
 }
 
 struct SingleStatisticCard: HTMLComponent, AttributeNode {
 
-    let stats: TemplateValue<SingleStatisticCardContent>
+    let title: HTML
+    let mainContent: HTML
+    
     var attributes: [HTMLAttribute] = []
 
     var body: HTML {
         Card {
-            Text {
-                stats.title
-            }
-            .text(color: .muted)
-
-            Text {
-                stats.mainContent
-            }
-            .text(color: .dark)
-            .style(.lead)
-            .font(style: .bold)
-
-            Text {
-                stats.extraContent
-            }
-            .text(color: .muted)
-
-            Unwrap(stats.extraContent) { extraContent in
-                Text {
-                    extraContent
-                }
+            Text { title }
                 .text(color: .muted)
-            }
+
+            Text { mainContent }
+                .text(color: .dark)
+                .style(.lead)
+                .font(style: .bold)
+
+//            IF(extraContent != nil) {
+//                Text { extraContent ?? "" }
+//                    .text(color: .muted)
+//            }
         }
         .add(attributes: attributes)
     }
 
     func copy(with attributes: [HTMLAttribute]) -> SingleStatisticCard {
-        .init(stats: stats, attributes: attributes)
+        .init(title: title, mainContent: mainContent, attributes: attributes)
     }
 }
